db68f3874d22ebe6b6f54e44cd448f99f8d0cd05
Mehmet Mevlevi <60301417+Mehmet-pro@users.noreply.github.com>
2021-09-24 15:52:10 +0300

    change json dir

--- analytics/views.py
+++ analytics/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests,json
+import requests,json,os,sys
 import dload
 from django.utils.safestring import SafeString
 from .getapi import getdata
 
 def index(request):
     getdata.get()
-    response = open('C:/Users/Mehmet/Desktop/COVID19/covid19/data.json')
+    os.chdir('../covid19')
+    response = open(os.path.join(os.getcwd(),'data.json'))
     responses = json.load(response)
     curl = 'https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json'
     flag = dload.json(curl)
