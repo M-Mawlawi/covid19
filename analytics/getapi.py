4a678163071aac6da931f3aa28e5f944e21b40e3
Mehmet Mevlevi <60301417+Mehmet-pro@users.noreply.github.com>
2021-09-24 15:30:00 +0300

    change flag api

--- analytics/getapi.py
+++ analytics/getapi.py
@@ -17,6 +17,21 @@ class getdata():
         df = df.rename(columns={"Name":"Region","index":"Name","Deaths - newly reported in last 24 hours":"Flag","Deaths - newly reported in last 7 days per 100000 population":"Deaths_newly_reported_in_last_24_hours","Deaths - newly reported in last 7 days":"Deaths_newly_reported_in_last_7_days_per_100000_population","Deaths - cumulative total per 100000 population":"Deaths_newly_reported_in_last_7_days","Deaths - cumulative total":"Deaths_cumulative_total_per_100000_population","Cases - newly reported in last 24 hours":"Deaths_cumulative_total","Cases - newly reported in last 7 days per 100000 population":"Cases_newly_reported_in_last_24_hours","Cases - newly reported in last 7 days":"Cases_newly_reported_in_last_7_days_per_100000_population","Cases - cumulative total per 100000 population":"Cases_newly_reported_in_last_7_days","Cases - cumulative total":"Cases_cumulative_total_per_100000_population","WHO Region":"Cases_cumulative_total"})
         df.at[0,'Region'] = 'Global'
         df.at[34,'Name'] = "Palestine, State of"
+        df.at[1,'Name'] = "United States"
+        df.at[8,'Name'] = "Iran, Islamic Republic of"
+        df.at[57,'Name'] = "Bolivia, Plurinational State of"
+        df.at[122,'Name'] = "Congo, the Democratic Republic of the"
+        df.at[70,'Name'] = "Venezuela, Bolivarian Republic of"
+        df.at[206,'Name'] = "Tanzania, United Republic of"
+        df.at[121,'Name'] = "Côte d'Ivoire"
+        df.at[4,'Name'] = "United Kingdom"
+        df.at[227,'Name'] = "Korea, Democratic People's Republic of"
+        df.at[78,'Name'] = "Korea, Republic of"
+        df.at[24,'Name'] = "Czech Republic"
+        df.at[15,'Name'] = "Macedonia, the Former Yugoslav Republic of"
+        df.at[81,'Name'] = "Moldova, Republic of"
+        
+        
 
         js = df.to_json(orient= 'records')
         with open(jsondata,'w') as file:
--- analytics/views.py
+++ analytics/views.py
@@ -9,6 +9,6 @@ def index(request):
     getdata.get()
     response = open('C:/Users/Mehmet/Desktop/COVID19/covid19/data.json')
     responses = json.load(response)
-    curl = 'https://restcountries.eu/rest/v2/all'
+    curl = 'https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json'
     flag = dload.json(curl)
     return render(request,"index.html",{"response":responses,"flag":flag})
